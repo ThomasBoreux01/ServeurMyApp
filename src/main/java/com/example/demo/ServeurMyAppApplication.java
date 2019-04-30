19d07defda2ddf0664758e016e22098f14383842
Thomas <thomas.62500@hotmail.fr>
2019-04-30 12:57:36 +0200

    ajout authentification avec MySQL (user+role)

--- src/main/java/com/example/demo/ServeurMyAppApplication.java
+++ src/main/java/com/example/demo/ServeurMyAppApplication.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 import com.example.demo.entity.Produit;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
 import com.example.demo.repository.ProduitRepository;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServeurMyAppApplication {
@@ -13,11 +19,27 @@ public class ServeurMyAppApplication {
 
         ConfigurableApplicationContext ctx = SpringApplication.run(ServeurMyAppApplication.class, args);
 
-        /*ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
+        ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
 
         produitRepository.save(new Produit("Livre",60,20));
         produitRepository.save(new Produit("Cahier",200,5.25f));
-        produitRepository.save(new Produit("Stylo",500,2.10f));*/
+        produitRepository.save(new Produit("Stylo",500,2.10f));
+
+        RoleRepository roleRepository=ctx.getBean(RoleRepository.class);
+        Role roleUser=new Role("ROLE_USER");
+        Role roleAdmin=new Role("ROLE_ADMIN");
+
+        roleRepository.save(roleUser);
+        roleRepository.save(roleAdmin);
+
+        UserRepository userRepository=ctx.getBean(UserRepository.class);
+        User user=new User("user","passworduser",true);
+        user.setRoles(Arrays.asList(roleUser));
+        userRepository.save(user);
+        User admin=new User("admin","passwordadmin",true);
+        admin.setRoles(Arrays.asList(roleUser,roleAdmin));
+        userRepository.save(admin);
     }
+
 
 }
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     protected void configure(HttpSecurity http) throws Exception{
         http.httpBasic().and().authorizeRequests().antMatchers("/api/**").hasRole("USER")
@@ -15,9 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("user").password("passworduser").roles("USER").and()
-                .withUser("admin").password("passwordadmin").roles("USER","ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
 
     }
 }
